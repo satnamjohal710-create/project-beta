ea490ab9a9fdee7109d4fa44060e6d7609ac1776
Satnam Singh <satnam@Mac.lan>
2026-05-21 21:42:45 +0530

    fix: clear vite base path and bypass azure oryx compilation via skip_app_build

--- beta-frontend/vite.config.js
+++ beta-frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' // or @vitejs/plugin-react
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './', // 🌟 This forces Vite to look for assets relative to the current folder!
+  plugins: [react()]
 })
